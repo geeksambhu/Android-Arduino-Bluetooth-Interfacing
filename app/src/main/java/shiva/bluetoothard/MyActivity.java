b0c16a8b767438bf35f7de5a30a42b08389781d7
Shiva Gaire <abiralsambhu@gmail.com>
2015-11-30 00:12:37 +0545

    Initial commit

---
+++ app/src/main/java/shiva/bluetoothard/MyActivity.java
@@ -0,0 +1,16 @@
+package shiva.bluetoothard;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by shiva on 11/28/15.
+ */
+public class MyActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       setContentView(R.layout.activity_main);
+    }
+}
